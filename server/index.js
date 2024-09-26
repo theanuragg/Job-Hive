12c41d7e971ad164fcc8c020614d912e113a6333
Vedansh Jain <vedansh.code@gmail.com>
2024-09-26 14:44:32 +0530

    add backend url from vercel

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
+    origin:true,
     credentials:true
 }
 
