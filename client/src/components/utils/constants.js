d24499f56be74446b730c30e3471f3e9736358d5
Vedansh Jain <vedansh.code@gmail.com>
2025-01-03 01:53:10 +0530

    gitignore file and render deployment

--- client/src/components/utils/constants.js
+++ client/src/components/utils/constants.js
@@ -1,7 +1,10 @@
-export const USER_API_END_POINT = 'http://localhost:8000/api/v1/user';
-export const JOB_API_END_POINT = 'http://localhost:8000/api/v1/job';
-export const APPLICATION_API_END_POINT = 'http://localhost:8000/api/v1/application';
-export const COMPANY_API_END_POINT = 'http://localhost:8000/api/v1/company';
+// let BASE_URL = 'http://localhost:8000/api/v1';
+let BASE_URL = 'https://job-hive.onrender.com/'
+
+export const USER_API_END_POINT = `${BASE_URL}/user`;
+export const JOB_API_END_POINT = `${BASE_URL}/job`;
+export const APPLICATION_API_END_POINT = `${BASE_URL}/application`;
+export const COMPANY_API_END_POINT = `${BASE_URL}/company`;
 
 // export const USER_API_END_POINT = 'https://job-hive-amber.vercel.app/api/v1/user';
 // export const JOB_API_END_POINT = 'https://job-hive-amber.vercel.app/api/v1/job';
