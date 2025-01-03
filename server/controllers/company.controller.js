74d737b96ba0ecf0591e8587130f9bdb5a86513d
Aditya <adityasahoo652@gmail.com>
2025-01-03 19:05:20 +0530

    feat: refactor  controllers to use asyncError middleware for error handling

--- server/controllers/company.controller.js
+++ server/controllers/company.controller.js
@@ -1,99 +1,72 @@
 import { Company } from "../models/company.model.js";
 import getDataUri from "../utils/datauri.js";
 import cloudinary from "../utils/cloudinary.js";
+import ErrorHandler from "../utils/error.js"
+import { asyncError } from "../middlewares/error.js"
 
-export const registerCompany = async (req, res) => {
-    try {
-        const { companyName } = req.body;
-        if (!companyName) {
-            return res.status(400).json({
-                message: "Company name is required.",
-                success: false
-            });
-        }
-        let company = await Company.findOne({ name: companyName });
-        if (company) {
-            return res.status(400).json({
-                message: "You can't register same company.",
-                success: false
-            })
-        };
-        company = await Company.create({
-            name: companyName,
-            userId: req.id
-        });
+export const registerCompany = asyncError(async (req, res, next) => {
+    const { companyName } = req.body;
+    if (!companyName) {
+        return next(new ErrorHandler("Company name is required.", 400));
+    }
+    let company = await Company.findOne({ name: companyName });
+    if (company) {
+        return next(new ErrorHandler("You can't register same company.", 400));
+    };
+    company = await Company.create({
+        name: companyName,
+        userId: req.id
+    });
 
-        return res.status(201).json({
-            message: "Company registered successfully.",
-            company,
-            success: true
-        })
-    } catch (error) {
-        // console.log(error);
+    return res.status(201).json({
+        message: "Company registered successfully.",
+        company,
+        success: true
+    })
+});
+export const getCompany = asyncError(async (req, res, next) => {
+    const userId = req.id; // logged in user id
+    const companies = await Company.find({ userId });
+    if (!companies) {
+        return next(new ErrorHandler("Companies not found.", 404));
     }
-}
-export const getCompany = async (req, res) => {
-    try {
-        const userId = req.id; // logged in user id
-        const companies = await Company.find({ userId });
-        if (!companies) {
-            return res.status(404).json({
-                message: "Companies not found.",
-                success: false
-            })
-        }
-        return res.status(200).json({
-            companies,
-            success:true
-        })
-    } catch (error) {
-        // console.log(error);
-    }
-}
+    return res.status(200).json({
+        companies,
+        success:true
+    })
+});
 // get company by id
-export const getCompanyById = async (req, res) => {
-    try {
-        const companyId = req.params.id;
-        const company = await Company.findById(companyId);
-        if (!company) {
-            return res.status(404).json({
-                message: "Company not found.",
-                success: false
-            })
-        }
-        return res.status(200).json({
-            company,
-            success: true
-        })
-    } catch (error) {
-        // console.log(error);
+export const getCompanyById = asyncError(async (req, res, next) => {
+    const companyId = req.params.id;
+    const company = await Company.findById(companyId);
+    if (!company) {
+        return next(new ErrorHandler("Company not found.", 404));
     }
-}
-export const updateCompany = async (req, res) => {
-    try {
-        const { name, description, website, location } = req.body;
- 
-        const file = req.file;
-        const fileUri = getDataUri(file);
-        const cloudResponse = await cloudinary.uploader.upload(fileUri.content);
-        const logo = cloudResponse.secure_url;
-    
-        const updateData = { name, description, website, location, logo };
+    return res.status(200).json({
+        company,
+        success: true
+    })
+});
+export const updateCompany = asyncError(async (req, res) => {
+    const { name, description, website, location } = req.body;
 
-        const company = await Company.findByIdAndUpdate(req.params.id, updateData, { new: true });
+    const file = req.file;
+    const fileUri = getDataUri(file);
+    const cloudResponse = await cloudinary.uploader.upload(fileUri.content);
+    const logo = cloudResponse.secure_url;
 
-        if (!company) {
-            return res.status(404).json({
-                message: "Company not found.",
-                success: false
-            })
-        }
-        return res.status(200).json({
-            message:"Company information updated.",
-            success:true
+    const updateData = { name, description, website, location, logo };
+
+    const company = await Company.findByIdAndUpdate(req.params.id, updateData, { new: true });
+
+    if (!company) {
+        return res.status(404).json({
+            message: "Company not found.",
+            success: false
         })
-
-    } catch (error) {
-        // console.log(error);
     }
-}
+    return res.status(200).json({
+        message:"Company information updated.",
+        success:true
+    })
+});
--- server/controllers/job.controller.js
+++ server/controllers/job.controller.js
@@ -1,102 +1,76 @@
 import { Job } from "../models/job.model.js";
+import { asyncError } from "../middlewares/error.js";
+import ErrorHandler from "../utils/error.js"
 
 // admin post krega job
-export const postJob = async (req, res) => {
-    try {
-        const { title, description, requirements, salary, location, jobType, experience, position, companyId } = req.body;
-        const userId = req.id;
+export const postJob = asyncError(async (req, res, next) => {
+    const { title, description, requirements, salary, location, jobType, experience, position, companyId } = req.body;
+    const userId = req.id;
 
-        if (!title || !description || !requirements || !salary || !location || !jobType || !experience || !position || !companyId) {
-            return res.status(400).json({
-                message: "Something is missing.",
-                success: false
-            })
-        };
-        const job = await Job.create({
-            title,
-            description,
-            requirements: requirements.split(","),
-            salary: Number(salary),
-            location,
-            jobType,
-            experienceLevel: experience,
-            position,
-            company: companyId,
-            created_by: userId
-        });
-        return res.status(201).json({
-            message: "New job created successfully.",
-            job,
-            success: true
-        });
-    } catch (error) {
-        // console.log(error);
-    }
-}
+    if (!title || !description || !requirements || !salary || !location || !jobType || !experience || !position || !companyId) {
+        return next(new ErrorHandler("Something is missing.", 400));
+    };
+    const job = await Job.create({
+        title,
+        description,
+        requirements: requirements.split(","),
+        salary: Number(salary),
+        location,
+        jobType,
+        experienceLevel: experience,
+        position,
+        company: companyId,
+        created_by: userId
+    });
+    return res.status(201).json({
+        message: "New job created successfully.",
+        job,
+        success: true
+    });
+});
 // student k liye
-export const getAllJobs = async (req, res) => {
-    try {
-        const keyword = req.query.keyword || "";
-        const query = {
-            $or: [
-                { title: { $regex: keyword, $options: "i" } },
-                { description: { $regex: keyword, $options: "i" } },
-            ]
-        };
-        const jobs = await Job.find(query).populate({
-            path: "company"
-        }).sort({ createdAt: -1 });
-        if (!jobs) {
-            return res.status(404).json({
-                message: "Jobs not found.",
-                success: false
-            })
-        };
-        return res.status(200).json({
-            jobs,
-            success: true
-        })
-    } catch (error) {
-        // console.log(error);
-    }
-}
+export const getAllJobs = asyncError(async (req, res, next) => {
+    const keyword = req.query.keyword || "";
+    const query = {
+        $or: [
+            { title: { $regex: keyword, $options: "i" } },
+            { description: { $regex: keyword, $options: "i" } },
+        ]
+    };
+    const jobs = await Job.find(query).populate({
+        path: "company"
+    }).sort({ createdAt: -1 });
+    if (!jobs) {
+        return next(new ErrorHandler("Jobs not found.", 404));
+    };
+    return res.status(200).json({
+        jobs,
+        success: true
+    })
+});
 // student
-export const getJobById = async (req, res) => {
-    try {
-        const jobId = req.params.id;
-        const job = await Job.findById(jobId).populate({
-            path:"applications"
-        });
-        if (!job) {
-            return res.status(404).json({
-                message: "Jobs not found.",
-                success: false
-            })
-        };
-        return res.status(200).json({ job, success: true });
-    } catch (error) {
-        // console.log(error);
-    }
-}
+export const getJobById = asyncError(async (req, res, next) => {
+    const jobId = req.params.id;
+    const job = await Job.findById(jobId).populate({
+        path:"applications"
+    });
+    if (!job) {
+        return next(new ErrorHandler("Job not found.", 404));
+    };
+    return res.status(200).json({ job, success: true });
+})
 // admin kitne job create kra hai abhi tk
-export const getAdminJobs = async (req, res) => {
-    try {
-        const adminId = req.id;
-        const jobs = await Job.find({ created_by: adminId }).populate({
-            path:'company',
-            createdAt:-1
-        });
-        if (!jobs) {
-            return res.status(404).json({
-                message: "Jobs not found.",
-                success: false
-            })
-        };
-        return res.status(200).json({
-            jobs,
-            success: true
-        })
-    } catch (error) {
-        // console.log(error);
-    }
-}
+export const getAdminJobs = asyncError(async (req, res, next) => {
+    const adminId = req.id;
+    const jobs = await Job.find({ created_by: adminId }).populate({
+        path:'company',
+        createdAt:-1
+    });
+    if (!jobs) {
+        return next(new ErrorHandler("Jobs not found.", 404));
+    };
+    return res.status(200).json({
+        jobs,
+        success: true
+    })
+});
