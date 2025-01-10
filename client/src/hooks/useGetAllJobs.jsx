2c1ce8a5a5b3b7716d592873462fefa411f0890f
Aditya <adityasahoo652@gmail.com>
2025-01-10 22:39:00 +0530

    fix: add loading state to useGetAllJobs for improved async handling

--- client/src/hooks/useGetAllJobs.jsx
+++ client/src/hooks/useGetAllJobs.jsx
@@ -3,6 +3,7 @@ import { JOB_API_END_POINT } from '../components/utils/constants'
 import axios from 'axios'
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { setJobLoading } from '@/redux/jobSlice'
 
 const useGetAllJobs = () => {
     const dispatch = useDispatch();
@@ -10,12 +11,15 @@ const useGetAllJobs = () => {
     useEffect(()=>{
         const fetchAllJobs = async () => {
             try {
+                dispatch(setJobLoading(true));
                 const res = await axios.get(`${JOB_API_END_POINT}/get?keyword=${searchedQuery}`,{withCredentials:true});
                 if(res.data.success){
                     dispatch(setAllJobs(res.data.jobs));
                 }
             } catch (error) {
                 // console.log(error);
+            }finally{
+                dispatch(setJobLoading(false));
             }
         }
         fetchAllJobs();
