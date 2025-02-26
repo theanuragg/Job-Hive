e03ff2e5083bbdafa6a35fa38fddf9e1d89cb3a0
Anurag Singh <anurags9693@gmail.com>
2025-02-26 23:28:23 +0500

    ui for jobspage

--- client/src/components/Jobs.jsx
+++ client/src/components/Jobs.jsx
@@ -74,9 +74,9 @@ const Jobs = () => {
           <div className="w-1/5">
             <FilterCard />
           </div>
-          <div className="flex-1 h-[88vh] overflow-y-auto pb-5">
+          <div className="flex-1 h-[88vh] pb-5">
             {filteredJobs.length <= 0 ? (
-              <span>No jobs found</span>
+              <span className="text-gray-700">No jobs found</span>
             ) : (
               <div className="grid grid-cols-3 gap-4">
                 {filteredJobs.map((job) => (
